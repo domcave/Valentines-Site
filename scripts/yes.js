0b6c376f3ffb99c542a1c418e6968076dddc7ea4
Dominick Dellecave <dominick@dellecave.com>
2023-12-31 16:40:07 -0500

    Finishing main yes page

---
+++ scripts/yes.js
@@ -0,0 +1,17 @@
+
+window.onload = function() {
+    
+    setTimeout(function() {
+        let p1 = document.getElementById('p1');
+        p1.style.opacity = 1;
+    }, 1000);
+    setTimeout(function() {
+        let p2 = document.getElementById('p2');
+        p2.style.opacity = 1;
+    }, 3000);
+    setTimeout(function() {
+        let prove = document.getElementById('prove');
+        prove.style.opacity = 1;
+    }, 5000);
+
+}
